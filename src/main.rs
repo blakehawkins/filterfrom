989b898ab5eb5cdadb01de0ff0a1306f32c2bda7
blakehawkins <blake.hawkins.11@gmail.com>
2020-02-15 08:16:31 +0000

    trim lines on print

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
+use oops::Oops;
 use stdinix::stdinix;
 
 #[paw::main]
 fn main(mut args: paw::Args) -> std::io::Result<()> {
-    let ban_path = args.nth(1).ok_or_else(|| std::io::Error::new(
-        std::io::ErrorKind::Other,
-        "Missing banlist",
-    ))?;
+    let ban_path = args.nth(1).oops("Missing banlist")?;
 
     let banset = std::fs::read_to_string(ban_path)?
         .lines()
@@ -14,7 +12,7 @@ fn main(mut args: paw::Args) -> std::io::Result<()> {
 
     stdinix(|line| {
         if !banset.contains(line.trim()) {
-            println!("{}", line);
+            println!("{}", line.trim());
         }
 
         Ok(())
